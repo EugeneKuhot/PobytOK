1bf80bf4063ebe0d20c50faa130f5089beac4fce
Yevhenii <kz.greenlinefamily@gmail.com>
2020-09-16 13:55:11 +0100

    Index(desktop)

---
+++ js/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function () {
+  let greetingsBlock = document.querySelector('.greetings__container');
+
+  function greetingBlockAnimation () {
+    greetingsBlock.classList.add('greetings__container--onload');
+  }
+
+  window.addEventListener('load', greetingBlockAnimation);
+})();
