7a9f9e2e2ea627cff293888a2136e84e6c0c76c6
Yevhenii <kz.greenlinefamily@gmail.com>
2020-09-21 15:17:25 +0100

    fix index

--- js/index-add-scripts.js
+++ js/index-add-scripts.js
@@ -8,4 +8,14 @@
     aboutUsAddText.style = "display: inline-block;";
     aboutUsAddBtn.style = "display: none;";
   });
+
+
+
+  let faqMobileHidden = document.querySelectorAll('.js-mobile-hidden');
+  let faqMobileShowBtn = document.querySelector('.faq__see-all');
+
+  faqMobileShowBtn.addEventListener('click', function () {
+    faqMobileHidden.forEach(el => el.style = "display: block");
+    faqMobileShowBtn.style = "display: none";
+  })
 })();
