f566640d08d30a05f550aac69bc63aadaa0d5bac
Yevhenii <kz.greenlinefamily@gmail.com>
2020-09-23 10:53:54 +0100

    login popup

--- js/goup.js
+++ js/goup.js
@@ -1,15 +1,26 @@
 'use strict';
 
 (function () {
-  var goTopBtn = document.querySelector('#scrollToTop');
+
+  let goTopBtn = document.querySelector('#scrollToTop');
+
+  let lastScrollTop = 0;
+  window.onscroll = onScroll;
+  function onScroll (e) {
+    let top = window.pageYOffset;
+    if (lastScrollTop > top) {
+      goTopBtn.classList.add('scrollToTop-show');
+    } else {
+      goTopBtn.classList.remove('scrollToTop-show');
+    }
+    lastScrollTop = top;
+  }
+
 
   function trackScroll () {
-    var scrolled = window.pageYOffset;
-    var coords = document.documentElement.clientHeight;
+    let scrolled = window.pageYOffset;
+    let coords = document.documentElement.clientHeight;
 
-    if (scrolled > coords - 500) {
-      goTopBtn.classList.add('scrollToTop-show');
-    }
     if (scrolled < coords - 500) {
       goTopBtn.classList.remove('scrollToTop-show');
     }
--- js/sign-in-popup.js
+++ js/sign-in-popup.js
@@ -5,6 +5,15 @@
   const regBtnHeader = document.querySelector('.header-nav__link.registration-btn');
   const loginBtnHeader = document.querySelector('.header-nav__link.login-btn');
   const signOverlay = document.querySelector('.sign-overlay');
+  const signCloseBtn = document.querySelector('.sign-wrap__btn');
+
+  function closePopup () {
+    signOverlay.classList.remove('sign-overlay--open');
+    signOverlay.classList.add('visually-hidden');
+  }
+
+  signCloseBtn.addEventListener('click', closePopup);
+
 
   const loginBtn = document.querySelector('.sign-wrap__login-btn');
   const regBtn = document.querySelector('.sign-wrap__reg-btn');
@@ -12,6 +21,8 @@
   const regWrap = document.querySelector('.registration-form');
 
   function getSignWrap (wrap, signBtn) {
+    signOverlay.classList.add('sign-overlay--open');
+
     loginWrap.classList.add('visually-hidden');
     regWrap.classList.add('visually-hidden');
     wrap.classList.remove('visually-hidden');
@@ -21,10 +32,21 @@
     signBtn.classList.add('sign-wrap__active-btn');
   }
 
-
   function getSignPopup (signWrap, signBtn) {
     signOverlay.classList.remove('visually-hidden');
-    getSignWrap(signWrap, signBtn)
+    getSignWrap(signWrap, signBtn);
+
+    document.addEventListener('keydown', function (e) {
+      if (e.keyCode === 27) {
+        closePopup();
+      }
+    });
+
+    document.addEventListener('click', function (e) {
+      if (e.target === signOverlay) {
+        closePopup();
+      }
+    })
   };
 
   regBtnHeader.addEventListener('click', function () {
