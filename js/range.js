77ebb88e7425782cff35d03ebdae45db6fbf7854
Yevhenii <kz.greenlinefamily@gmail.com>
2020-10-01 10:33:02 +0100

    Porperty page

--- js/range.js
+++ js/range.js
@@ -2,34 +2,33 @@
 
 (function() {
 
-  var parent = document.querySelector(".range-slider");
+  var parent = document.querySelectorAll(".range-slider");
   if(!parent) return;
 
-  var
-    rangeS = parent.querySelectorAll("input[type=range]"),
-    numberS = parent.querySelectorAll("input[type=number]");
+  var rangeS1 = parent[0].querySelectorAll("input[type=range]"),
+    numberS1 = parent[0].querySelectorAll("input[type=number]");
 
-  rangeS.forEach(function(el) {
+  rangeS1.forEach(function(el) {
     el.oninput = function() {
-      var slide1 = parseFloat(rangeS[0].value),
-        slide2 = parseFloat(rangeS[1].value);
+      var slide1 = parseFloat(rangeS1[0].value),
+        	slide2 = parseFloat(rangeS1[1].value);
 
       if (slide1 > slide2) {
-        [slide1, slide2] = [slide2, slide1];
+				[slide1, slide2] = [slide2, slide1];
         // var tmp = slide2;
         // slide2 = slide1;
         // slide1 = tmp;
       }
 
-      numberS[0].value = slide1;
-      numberS[1].value = slide2;
+      numberS1[0].value = slide1;
+      numberS1[1].value = slide2;
     }
   });
 
-  numberS.forEach(function(el) {
+  numberS1.forEach(function(el) {
     el.oninput = function() {
-      var number1 = parseFloat(numberS[0].value),
-        number2 = parseFloat(numberS[1].value);
+			var number1 = parseFloat(numberS2[0].value),
+					number2 = parseFloat(numberS2[1].value);
 
       if (number1 > number2) {
         var tmp = number1;
@@ -44,38 +43,67 @@
   });
 
 
+var rangeS2 = parent[1].querySelectorAll("input[type=range]"),
+    numberS2 = parent[1].querySelectorAll("input[type=number]");
+
+  rangeS2.forEach(function(el) {
+    el.oninput = function() {
+      var slide1 = parseFloat(rangeS2[0].value),
+        	slide2 = parseFloat(rangeS2[1].value);
+
+      if (slide1 > slide2) {
+				[slide1, slide2] = [slide2, slide1];
+        // var tmp = slide2;
+        // slide2 = slide1;
+        // slide1 = tmp;
+      }
+
+      numberS2[0].value = slide1;
+      numberS2[1].value = slide2;
+    }
+  });
+
+  numberS2.forEach(function(el) {
+    el.oninput = function() {
+			var number1 = parseFloat(numberS2[0].value),
+					number2 = parseFloat(numberS2[1].value);
+
+      if (number1 > number2) {
+        var tmp = number1;
+        numberS2[0].value = number2;
+        numberS2[1].value = tmp;
+      }
+
+      rangeS2[0].value = number1;
+      rangeS2[1].value = number2;
+
+    }
+  });
+
+
+
+
 
 
   const allRanges = document.querySelectorAll(".range-slider");
   allRanges.forEach(wrap => {
-    const range = wrap.querySelector(".range-wrap__max-range");
-    const bubble = wrap.querySelector(".range-value-output");
+  const range = wrap.querySelector(".range-wrap__max-range");
+  const bubble = wrap.querySelector(".range-value-output");
 
-    range.addEventListener("input", () => {
-      setBubble(range, bubble);
-    });
+  range.addEventListener("input", () => {
     setBubble(range, bubble);
+  });
+  setBubble(range, bubble);
   });
 
   function setBubble(range, bubble) {
-    const min = range.min ? range.min : 0;
-    const max = range.max ? range.max : 100;
-    const val = range.value;
-    const bubbleXPercentile = Number(((val - min) * 100) / (max - min));
+  const min = range.min ? range.min : 0;
+  const max = range.max ? range.max : 100;
+  const val = range.value;
+  const bubbleXPercentile = Number(((val - min) * 100) / (max - min));
 
-    bubble.style.left = `calc(${bubbleXPercentile}% + (${20 - bubbleXPercentile * 0.15}px))`;
-  }
-
-
-
-
-
-
-
-
-
-
-
+  bubble.style.left = `calc(${bubbleXPercentile}% + (${20 - bubbleXPercentile * 0.15}px))`;
+}
 
 
 
