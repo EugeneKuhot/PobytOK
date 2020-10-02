052e853cf818bd6bf8097e29512a4da3e3c6d64b
Yevhenii <kz.greenlinefamily@gmail.com>
2020-10-02 16:59:20 +0100

    Property range

--- js/main-range.js
+++ js/main-range.js
@@ -1,51 +1,43 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-    // ======== Slider with two handles
 
-    const discountSlider = document.getElementById('m-slider');
+  // ======== Slider with one handle
 
-    noUiSlider.create(discountSlider, {
-        start: 50,
-        tooltips: true,
-        connect: [true, false],
-        step: 1,
-        range: {
-            'min': 0,
-            'max': 100
-        },
-        format: {
-            to: function (value) {
-                return parseInt(value);
-            },
-            from: function (value) {
-                return parseInt(value);
-            }
-        }
-    });
+  const priceSlider = document.getElementById('r-slider');
 
-    discountSlider.noUiSlider.on('change', (values, handle) => {
-        goSearch();
-    });
-
-    // ======== Search filters
-
-    function goSearch() {
-        let winHref = window.location.href.split('?')[0];
-        winHref += `?pricerange=${priceSlider.noUiSlider.get()}`;
-        winHref += `&mindiscount=${discountSlider.noUiSlider.get()}`;
-        window.location.href = winHref;
+  noUiSlider.create(priceSlider, {
+    start: [0, 10000],
+    tooltips: true,
+    connect: true,
+    padding: 6,
+    range: {
+      'min': 0,
+      'max': 10000
+    },
+    step: 1,
+    format: {
+      // 'to' the formatted value. Receives a number.
+      to: function (value) {
+        return value;
+      },
+      // 'from' the formatted value.
+      // Receives a string, should return a number.
+      from: function (value) {
+        return value;
+      }
     }
+  });
 
-    // ======== Slider set
 
-    const params = new URLSearchParams(window.location.search);
-    const minDiscount = params.get("mindiscount");
-    const priceRange = params.get("pricerange");
+  // ======== Slider set
 
-    if (minDiscount) {
-        discountSlider.noUiSlider.set(parseInt(minDiscount));
-    }
-    if (priceRange) {
-        priceSlider.noUiSlider.set(priceRange.split(','));
-    }
+  const params = new URLSearchParams(window.location.search);
+  const priceRange = params.get("pricerange");
+  let priceFrom = document.querySelector('#price-from');
+
+  if (priceRange) {
+    priceSlider.noUiSlider.set(priceRange.split(','));
+    priceFrom.value = priceSlider.noUiSlider.get();
+  }
+
+
 });
